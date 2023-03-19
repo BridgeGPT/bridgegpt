fefca7ece7f29cd35edb9d055e95504138c83656
gdassori <guido.dassori@gmail.com>
2023-03-19 12:04:11 +0100

    system_service fixes, prompt fixes

--- bridgegpt/system_service.py
+++ bridgegpt/system_service.py
@@ -9,35 +9,18 @@ class SystemService:
     def is_valid_json(self, payload):
         try:
             obj = json.loads(payload)
-            return isinstance(obj, dict) and "action" in obj and "id" in obj
+            return isinstance(obj, dict) and "action" in obj and "id" in obj and "from" in obj
         except ValueError:
             return False
 
     def execute_command(self, command_id, command):
-        # Check if command uses pipes
-        if "|" in command:
-            # Split the command by pipes
-            commands = command.split("|")
-            # Execute the commands using subprocess.PIPE to redirect output
-            # from one command to the input of the next command
-            process = None
-            for cmd in commands:
-                cmd = cmd.strip()
-                if process is None:
-                    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, executable="/bin/bash")
-                else:
-                    process = subprocess.Popen(cmd.split(), stdin=process.stdout, stdout=subprocess.PIPE, executable="/bin/bash")
-            output, _ = process.communicate()
-            response = {"id": command_id, "response": output.decode().strip()}
-            return json.dumps(response)
-        else:
-            try:
-                # Execute the command using subprocess.check_output
-                output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, executable="/bin/bash").decode().strip()
-                response = {"id": command_id, "response": output}
-            except subprocess.CalledProcessError as e:
-                response = {"id": command_id, "error": e.output.decode().strip()}
-            return json.dumps(response)
+        try:
+            # Execute the command using subprocess.check_output
+            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, executable="/bin/bash").decode().strip()
+            response = {"id": command_id, "response": output, "from": "BridgeGPT"}
+        except subprocess.CalledProcessError as e:
+            response = {"id": command_id, "error": e.output.decode().strip(), "from": "BridgeGPT"}
+        return json.dumps(response)
 
     def process_json(self, json_string):
         try:
@@ -47,6 +30,6 @@ class SystemService:
                 command = payload["action"]
                 return self.execute_command(command_id, command)
             else:
-                return json.dumps({"error": "Invalid JSON payload."})
+                return json.dumps({"error": "Invalid JSON payload.", "from": "BridgeGPT"})
         except json.JSONDecodeError:
-            return json.dumps({"error": "Invalid JSON payload."})
+            return json.dumps({"error": "Invalid JSON payload.", "from": "BridgeGPT"})
