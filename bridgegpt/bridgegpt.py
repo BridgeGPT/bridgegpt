333e190710a81b1ee95893a3d38718e6d4a3b731
gdassori <guido.dassori@gmail.com>
2023-03-19 09:53:53 +0100

    DRAFT - BridgeGPT - A bridge to the real world for ChatGPT

--- bridgegpt/bridgegpt.py
+++ bridgegpt/bridgegpt.py
@@ -1,9 +1,12 @@
 import click
+
+from bridgegpt.system_service import SystemService
 from .openai_service import OpenAIService
 import os
 
 
 openai_service = OpenAIService()
+system_service = SystemService()
 
 # Get the absolute path to the directory containing this file
 dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -23,10 +26,16 @@ def chat():
     openai_service.prompt = f"Conversation:\nUser: {context}\nAI:"
 
     while True:
-        user_input = input("You: ")
+        user_input = click.prompt("\nYou")
         ai_response = openai_service.generate_response(user_input)
-        click.echo(f"AI: {ai_response}")
 
+        if system_service.is_valid_json(ai_response):
+            click.echo(f"\nAI (hidden): {ai_response}")
+            bridgegpt_response = system_service.process_json(ai_response)
+            click.echo(f"BridgeGPT (hidden): {bridgegpt_response}")
+            ai_response = openai_service.generate_response(bridgegpt_response)
+
+        click.echo(f"\nAI: {ai_response}")
         if ai_response.endswith("Goodbye!"):
             break
 
--- bridgegpt/openai_service.py
+++ bridgegpt/openai_service.py
@@ -1,3 +1,5 @@
+import getpass
+
 import openai
 import os
 
@@ -5,25 +7,33 @@ import os
 class OpenAIService:
     def __init__(self):
         self.prompt = ""
+        self.context = []
+        self.model_id = "gpt-3.5-turbo"
         openai.api_key = os.environ["OPENAI_API_KEY"]
 
-    def generate_response(self, user_input):
-        self.prompt += f"{user_input}\nAI:"
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": self.prompt},
-                {"role": "user", "content": user_input}
-            ]
-        )
-        ai_response = response.choices[0].message.content.strip()
-        self.prompt += f"{ai_response}\nUser:"
-        return ai_response
+        # Get the path to the base prompt file
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        prompt_path = os.path.join(script_dir, "base_prompt.txt")
 
-    def list_models(self):
-        models = openai.Model.list()
-        model_list = []
-        for model in models["data"]:
-            print(model)
-            model_list.append(f"{model['id']}")
-        return "\n".join(model_list)
+        # Read the base prompt from the file
+        with open(prompt_path, "r") as f:
+            self.prompt = f.read().strip()
+
+        self.context.append({"role": "system", "content": self.prompt})
+
+    def generate_response(self, user_input):
+        self.context.append({"role": "user", "content": user_input})
+        response = openai.ChatCompletion.create(
+            model=self.model_id,
+            temperature=0.9,
+            max_tokens=2048,
+            n=1,
+            stop=None,
+            presence_penalty=0,
+            frequency_penalty=0,
+            messages=self.context
+        )
+        msg = response.choices[0].message
+        ai_response = msg.content.strip()
+        self.context.append({"role": msg.role, "content": ai_response})
+        return ai_response
